d8241f740b6dfcd61b71128ce00ba216bcdf9bc5
aaletov <52921276+aaletov@users.noreply.github.com>
2020-01-07 15:24:00 +0500

    Я все поломал впизду

---
+++ aatelebot/maincycle.py
@@ -0,0 +1,37 @@
+import time
+from tg_bot import *
+import os.path
+
+
+path_ = os.getcwd() + '//' + 'aatelebot' + '//'
+last_upd = time()
+groups = get_groups_list()
+
+while True:
+    with open(path_ + 'filter.txt', 'r', encoding = 'utf-8') as file:
+        groups = eval(file.read() )
+
+    if (time() - last_upd)//3600 > 0:
+        groups = update_groups_list(groups)
+
+    updates, groups = get_vk_updates(groups)
+
+    with open(path_ + 'updated.txt', 'w', encoding = 'utf-8') as file:
+        file.write(str(groups) )
+
+    updates = get_video_info(updates)
+
+    with open(path_ + 'filter.txt', 'r', encoding = 'utf-8') as file:
+        post_filter = eval(file.read() )[0].keys()
+
+    for post in updates:
+        try:
+            sendPost(post, '-1001430319971')
+            if str(post.group_id) not in post_filter:
+                sendPost(post, '-1001185715274')
+            sleep(1)
+        except:
+                print('API_Error\n', post, '\n\n')
+                raise IndexError
+
+    sleep(600)
---
+++ aatelebot/tg_bot.py
@@ -0,0 +1,314 @@
+import vk
+import requests
+import version
+import os
+import json
+from time import time, sleep
+from copy import deepcopy, copy
+
+#vk_token = '0fd3e5674fbea380f6e011336a3e526fcbf950d3deab8b7dc4c6dff05fb166cac329e91e07715b3b4c206' #елкин
+vk_token = 'da85fb5129c0a72383163ea16171aa2b3d4679595ab28631e1fbe53fa2599f6049c1aae04875b1f65a683' #чепига
+tg_token = '751858938:AAG4i-Ec8VfdnnQcSOGOOCdVwl6jIH1cv6Y'
+tgapi_url = "https://api.telegram.org/bot{}/".format(tg_token)
+path_ = os.getcwd() + '//' + 'aatelebot' + '//'
+
+session = vk.Session(access_token = vk_token)
+vkapi = vk.API(session)
+v = version.get_version()
+
+class VkPost():
+    def __init__(self, group_name, post = None):
+        #
+        #post - объект, возвращаемый методом wall.get в поле 'items'
+        #
+        self.group_name = group_name
+        self.text = ''
+        self.error = 0
+        self.photos = []
+        self.videos = []
+        self.docs = []
+        self.group_id = post['owner_id']
+
+        if post != None:
+            if post['marked_as_ads'] == 1:
+                self.text = ''
+                return
+
+            if post.get('text') != None:
+                self.text += post.get('text') 
+
+            attachments = post.get('attachments')
+            if attachments != None:
+                for attachment in attachments:
+                    type = attachment['type']
+                    if type == 'photo':
+                        self.photos.append(attachment['photo']['sizes'][-1]['url'])
+
+                    elif type == 'video':
+                        owner_id = str( attachment['video']['owner_id'] )
+                        video_id = str( attachment['video']['id'] )
+                        access_key = str( attachment['video']['access_key'] )
+                        video = owner_id + '_' + video_id + '_' + access_key 
+                        self.videos.append(video)
+                        
+                    elif type == 'doc':
+                        self.docs.append(attachment['doc']['url'])
+
+    def __str__(self):
+            return str([self.group_name, self.text, str(self.photos), str(self.videos), str(self.docs)])
+
+
+def get_post_object(url, v = v):
+#Получение объетка по url
+
+    str1 = url.find('wall')
+    posts = url[str1+4:]
+    str1 = posts.find('_')
+    owner_id = posts[:str1]
+    post = vkapi.wall.getById(owner_id = owner_id, posts = posts, v=v)
+    sleep(0.33)
+    group_name = vkapi.groups.getById(group_id = -int(owner_id), v=v)[0]['name']
+    try:
+        return VkPost(group_name, post = post[0])
+    except:
+        print(post)
+        raise IndexError 
+
+def get_groups_list(v=v):
+#Получение списка сообществ текущего пользователя
+
+    groups = vkapi.groups.get(v = v)
+    A = []
+    IDs = []
+
+    response = vkapi.groups.getById(group_ids = ','.join(map(str,groups['items'])), v=v)
+
+    Names = [group['name'] for group in response]
+
+    for i in range(groups['count']):
+        IDs.append(-int(groups['items'][i]) )
+    for i in range(len(IDs)):
+        A.append({'group_id':IDs[i],'count':0, 'name':Names[i]})
+
+    groups = A 
+
+    return groups 
+
+def get_vk_updates(groups, v=v):
+    #Получение обновлений по имеющемуся списку и создание списка из VkPost объектов
+
+    vk_updates = []
+
+    for i in range(len(groups)):
+        try:
+            update = vkapi.wall.get(owner_id = groups[i]['group_id'], count = 15, v = v)    
+        except:
+            print('Group %s is blocked'%groups[i]['name'])
+        new_count = update['count'] - groups[i]['count']
+        if update['items'][0].get('is_pinned') != 1:
+            update_new = update['items'][:new_count]
+        else:
+            update_new = update['items'][1:new_count+1]
+        posts = []
+        for post in update_new:
+            copy_history = post.get('copy_history')
+            if copy_history == None:
+                posts.append(VkPost(groups[i]['name'], post = post ) )
+        
+        posts.reverse()
+        vk_updates += posts
+        groups[i]['count'] = update['count']
+        sleep(0.33)
+
+    return(vk_updates , groups)
+
+def get_video_info(vkp_list, v=v):
+    #Разовое получение информации о всех видео из апдейта
+
+    urls = []
+    videos_list = []
+    posts = []
+    items = []
+    for i in range(len(vkp_list) ):
+        videos = vkp_list[i].videos
+        if videos != []:
+            videos_list += videos
+            posts.append(i)
+    
+    for i in range(len(videos_list)//100):
+        part = videos_list[i*100:(i+1)*100]
+        response = vkapi.video.get(videos = ','.join(part), v=v )
+        items += response['items']
+
+    response = vkapi.video.get(videos = ','.join(videos_list[len(videos_list)//100*100: ] ), v=v )
+    items += response['items']
+
+    for item in items:
+        try:
+            urls.append(item['player'])
+        except:
+            urls.append(None)
+
+    for i in posts:
+        for j in range(len(vkp_list[i].videos)):
+            if urls[0].find('youtube') == -1 and urls[0].find('vimeo') == -1 and urls[0].find('rutube') == -1 and urls[0].find('yandex') == -1 :
+                vkp_list[i].videos[j] = urls.pop(0)
+            else:
+                vkp_list[i].videos.pop(0)
+                vkp_list[i].text += '\n' + urls.pop(0)
+
+    return vkp_list
+
+def sendPost(post, chat_id, tgapi_url = tgapi_url):
+    att_count = len(post.videos + post.photos + post.docs)
+    params = {'chat_id': chat_id}
+    if post.text != '' and att_count == 0:
+        sendMessage(post.group_name + '\n\n' + post.text, params)
+
+    if post.photos != []:
+        sendPhotos(post.photos, params, post.group_name, post.text )
+        post.text = ''
+
+    if post.videos != []:
+        sendVideos(post.videos, params, post.group_name, post.text)
+        post.text = ''
+
+    if post.docs != []:
+        sendDocuments(post.docs, params, post.group_name, post.text)
+
+def sendMessage(text, params, tgapi_url = tgapi_url):
+    method = 'sendMessage'
+    params.update({'text':text})
+    return(requests.post(tgapi_url + method, params))
+
+def sendPhotos(photos, params, group_name, text = '', tgapi_url = tgapi_url ):
+    if len(photos) == 1:
+        method = 'sendPhoto'
+        params.update({'photo':photos[0], 'caption': group_name + '\n\n' + text})
+
+    else:
+        method = 'sendMediaGroup'
+        media_array = [
+                    {
+                        'type':'photo',
+                        'media': photos[0], 
+                        'caption': group_name + '\n\n' + text 
+                        }
+                    ]
+
+        for photo in photos[1:]:
+            media_array.append(
+                {
+                    'type':'photo', 
+                    'media':photo 
+                    }
+              )
+          
+        params.update({'media':json.dumps(media_array) } )
+
+    return(requests.post(tgapi_url + method, params))
+
+def sendVideos(videos, params, group_name, text = '', tgapi_url = tgapi_url):
+    if len(videos) == 1:
+        method = 'sendVideo'
+        version.download_video_vk(videos[0], 'file')
+        files = {'video': open(path_ + 'file.mp4', 'rb')} #multipart/form-data
+        params.update({'supports_streaming':True, 'caption':group_name + '\n\n' + text})
+        try:
+            response = requests.post(tgapi_url + method, params = params, files = files )
+        except:
+            response = None
+            print('Upload Error')
+
+        return response
+
+    else:
+        method = 'sendMediaGroup'
+        media_array = []
+        files = {}
+
+        for i in range(len(videos)):
+            filename = 'file' + str(i)
+            version.download_video_vk(videos[i], filename)
+            media_array.append(
+                {
+                    'type':'video', 
+                    'media':'attach://' + filename + '.mp4', 
+                    'supports_streaming': True 
+                    } 
+                )
+            files.update({filename + '.mp4': open(path_ + filename + '.mp4', 'rb') } )
+
+        media_array[0].update({'caption':group_name + '\n\n' + text} )
+        params.update({'media':json.dumps(media_array)} )
+        try:
+            response = requests.post(tgapi_url + method, params = params, files = files)
+        except:
+            response = None
+            print('Upload Error')
+
+        return response
+
+def sendDocuments(docs, params, group_name, text = '', tgapi_url = tgapi_url):
+    method = 'sendDocument'
+    params1 = copy(params)
+    params1.update({'document':docs[0], 'caption': group_name + '\n\n' + text})
+    responses = [requests.post(tgapi_url + method, params1)]
+    for doc in docs[1:]:
+        params1 = copy(params)
+        params1.update({'document':doc})
+        print(params1)
+        responses.append(requests.post(tgapi_url + method, params1) )
+
+    return responses 
+
+def update_groups_list(groups, v=v):
+    new_list = get_groups_list()
+    new_ids = set([i['group_id'] for i in new_list] )
+    old_ids = set([i['group_id'] for i in groups] )
+    inter = new_ids & old_ids
+    sub = new_ids - inter
+    leaved = old_ids - inter
+    for i in range(len(groups ) ):
+        if groups[i]['group_id'] in leaved:
+            groups[i].pop()
+    
+    for i in range(len(new_list )):
+        if new_list[i]['group_id'] in sub:
+            groups.append(new_list[i] )
+
+    return groups
+
+if __name__ == '__main__':
+    last_upd = time()
+    groups = get_groups_list()
+
+    while True:
+        with open(path_ + 'filter.txt', 'r', encoding = 'utf-8') as file:
+            groups = eval(file.read() )
+
+        if (time() - last_upd)//3600 > 0:
+            groups = update_groups_list(groups)
+
+        updates, groups = get_vk_updates(groups)
+
+        with open(path_ + 'updated.txt', 'w', encoding = 'utf-8') as file:
+            file.write(str(groups) )
+
+        updates = get_video_info(updates)
+
+        with open(path_ + 'filter.txt', 'r', encoding = 'utf-8') as file:
+            post_filter = eval(file.read() )[0].keys()
+
+        for post in updates:
+            try:
+                sendPost(post, '-1001430319971')
+                if str(post.group_id) not in post_filter:
+                    sendPost(post, '-1001185715274')
+                sleep(1)
+            except:
+                print('API_Error\n', post, '\n\n')
+                raise IndexError
+
+        sleep(600)
+
