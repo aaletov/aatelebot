5092382d98becad46a8f6c240f9b956ec1cfea89
aaletov <52921276+aaletov@users.noreply.github.com>
2020-01-19 13:05:30 +0500

    1

--- aatelebot/test.py
+++ aatelebot/test.py
@@ -1,2 +1,2 @@
 import version 
-version.download_video_vk('https://cs522400.vkuservideo.net/18/ee9NDM2OjkyMTY_/videos/5191f0405f.720.mp4', 'file')
+version.download_video_vk('https://cs573204.vkuservideo.net/10/efcPjIxMjg-MjM/videos/ef06815817.720.mp4', 'file')
